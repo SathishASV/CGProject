e96eeb4bb4c4b7689d0d6a19e6057c239f7479a4
SathishASV <alakuntlasathish12345@gmail.com>
2022-04-15 12:52:52 +0530

    commit1

---
+++ src/main/java/com/springgit/DemoSpringForGitApplication.java
@@ -0,0 +1,13 @@
+package com.springgit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoSpringForGitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoSpringForGitApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/springgit/DemoSpringForGitApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springgit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoSpringForGitApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
